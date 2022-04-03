02e938e9e1f6085fa6e55499ab4dc552d436ce1a
Geeth Wishkamal <41125548+geethwish@users.noreply.github.com>
2022-04-03 09:50:16 +0530

    login and register ui added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import Header from './components/header/Header';
 import Dashboard from './pages/dashboard/Dashboard';
@@ -8,20 +9,35 @@ import Register from './pages/register/Register';
 
 import './assets/scss/index.scss';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 
 function App() {
     return (
         <>
             <Router>
+
                 <div>
+
                     <Header />
+
                     <Routes>
+
                         <Route path="/" element={<Dashboard />} />
+
                         <Route path="/login" element={<Login />} />
+
                         <Route path="/register" element={<Register />} />
+
                     </Routes>
+
                 </div>
+
             </Router>
+
+            <ToastContainer />
 
         </>
 
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,212 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { Paper } from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { register, reset } from '../../features/auth/authSlice';
+import Spinner from '../../components/spinner/Spinner';
+
+
 
 function Register() {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const { user, isError, message, isLoading, isSuccess, } = useSelector((state) => state.auth);
+
+    useEffect(() => {
+
+        if (isError) {
+
+            toast.error(message)
+        }
+
+        if (isSuccess || (user && user.data)) {
+
+            navigate('/');
+
+        }
+
+        dispatch(reset());
+
+    }, [user, isError, message, isLoading, isSuccess, navigate, dispatch])
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = new FormData(event.currentTarget);
+
+        const password = data.get('password');
+        const confirmPassword = data.get('confirmPassword');
+        const name = data.get('name');
+        const email = data.get('email');
+
+        if (password !== confirmPassword) {
+
+            toast.error("password do not Match");
+
+        } else {
+
+            const userData = {
+                name,
+                email,
+                password,
+                confirmPassword
+            }
+
+            console.log(userData);
+
+            dispatch(register(userData))
+        }
+
+    };
+
+    if (isLoading) {
+
+        return <Spinner />
+
+    }
+
     return (
-        <div>Register</div>
+        <Paper
+            className="mt-10"
+            elevation={0}
+            sx={{
+                p: 3,
+                margin: 'auto',
+                maxWidth: 540,
+                flexGrow: 1,
+                backgroundColor: (theme) =>
+                    theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+            }}
+        >
+
+            <Container component="main" maxWidth="xs">
+
+                <Box
+                    sx={{
+                        marginTop: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+
+                    <Typography component="h1" variant="h5">
+                        Create an Account
+                    </Typography>
+
+                    <Box component="form" noValidate onSubmit={(e) => handleSubmit(e)} sx={{ mt: 3 }}>
+
+                        <Grid container spacing={2}>
+
+                            <Grid item xs={12}>
+
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="name"
+                                    required
+                                    fullWidth
+                                    id="name"
+                                    label="Name"
+                                    autoFocus
+                                />
+
+                            </Grid>
+
+                            <Grid item xs={12}>
+
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                />
+
+                            </Grid>
+
+                            <Grid item xs={12}>
+
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                />
+
+                            </Grid>
+
+                            <Grid item xs={12}>
+
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="confirmPassword"
+                                    label="Confirm Password"
+                                    type="password"
+                                    id="confirmPassword"
+                                    autoComplete="confirmPassword"
+                                />
+
+                            </Grid>
+
+                            <Grid item xs={12}>
+
+                                <FormControlLabel
+                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                    label="By creating account you agree the terms and conditions"
+                                />
+
+                            </Grid>
+
+                        </Grid>
+
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+
+                            Create Account
+
+                        </Button>
+
+                        <Grid container justifyContent="flex-end">
+
+                            <Grid item>
+
+                                <Link href="#" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+
+                            </Grid>
+
+                        </Grid>
+
+                    </Box>
+
+                </Box>
+
+            </Container>
+
+        </Paper>
     )
 }
 
